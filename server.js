8408ce1d2e15f3227fb03a214b0bbcc269cd6cdf
richerzhagen <jelle.richerzhagen@gmail.com>
2020-05-26 18:24:47 +0800

    added mongodb / api / websocket

--- server.js
+++ server.js
@@ -1,24 +1,108 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const { Server } = require('ws');
+const express = require("express");
+const app = express();
+var port = 3000;
+const router = express.Router();
+var crawlList = [];
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+//#region mongodb
+const mongoose = require("mongoose");
+// initialise mongodb connection
+mongoose.connect("mongodb://localhost:27017/nodedb");
+let db = mongoose.connection;
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wss = new Server({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+// check connection
+db.once("open", function () {
+  console.log("Connected to MongoDB");
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+// check for db errors
+db.on("error", function (err) {
+  console.log(err);
+});
+let schema = mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  },
+});
+var Model = mongoose.model("model", schema, "myCollection");
+// var doc1 = new Model({ url: "test", active: true });
+// doc1.save(function (err, doc) {
+//   if (err) return console.error(err);
+//   console.log("Document inserted succussfully!");
+// });
+
+router.route("/fetch").get(function (req, res) {
+  Model.find({}, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
   });
-}, 1000);
+});
+
+// var query = { address: /^S/ };
+// db.collection("myCollection").find(query).toArray(function(err, result) {
+Model.find({}, function (err, result) {
+  if (err) {
+    console.log("error");
+    throw err;
+  }
+  console.log(result);
+  crawlList = result;
+  // db.close();
+});
+
+//#endregion
+
+//#region router
+
+app.use("/", router);
+
+router.get("/status", function (req, res) {
+  res.json({ status: "app is running" });
+});
+
+var server = app.listen(port, function () {
+  console.log("server is running on port " + port);
+});
+
+//#endregion
+
+//#region websocket
+const SocketServer = require("ws").Server;
+const wss = new SocketServer({ server });
+var connectedUsers = [];
+//init websocket ws and handle incomming request
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
+
+  ws.send(JSON.stringify({data: crawlList, status:"success"}));
+});
+
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
+
+// wss.on('connection', function connection(ws){
+//   console.log("connection ...");
+
+//   //on connect message
+//   wss.on('message', function incoming(message){
+//     console.log('received %s', message);
+//     connectedUsers.push(message);
+//   });
+
+//   ws.send('something');
+// });
+//#endregion
